8f5aed60da4f132708bf3e358ca56295e29fe591
Stackbit Code Editor <projects@stackbit.com>
2021-08-04 17:23:26 +0000

    gatsby-config.js: changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
         social: [
             {
                 name: `twitter`,
-                url: `https://twitter.com/narative`,
+                url: `https://twitter.com/knighttimesnews`,
             },
             {
                 name: `github`,
